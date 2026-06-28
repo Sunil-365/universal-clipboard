7871ec8066d46ab6bd356c43e2881adbc29abaef
Sunil Ohdar <mac@Sunils-MacBook-Air.local>
2026-06-28 10:29:11 +0530

    Updated ad network zoneId in sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11210276
+    "zoneId": 11210295
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
