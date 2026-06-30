c63690db1567da476018c92567ec12782461f483
Sunil Ohdar <mac@Sunils-MacBook-Air.local>
2026-06-30 17:20:06 +0530

    chore: clean up unused test and setup scripts

--- test-paddle-list.js
+++
@@ -1,6 +0,0 @@
-const { Paddle, Environment } = require('@paddle/paddle-node-sdk');
-const paddle = new Paddle('pdl_sdbx_apikey_01kwbejpt7272rfv9p4pd7kvs7_t0HEPa3ZqpGhERq9FZ7QFx_AUk', { environment: Environment.sandbox });
-async function run() {
-    console.log(await paddle.clientTokens.list());
-}
-run();
--- test-paddle.js
+++
@@ -1,3 +0,0 @@
-const { Paddle, Environment } = require('@paddle/paddle-node-sdk');
-const paddle = new Paddle('pdl_sdbx_apikey_01kwbejpt7272rfv9p4pd7kvs7_t0HEPa3ZqpGhERq9FZ7QFx_AUk', { environment: Environment.sandbox });
-console.log(Object.keys(paddle));
